0e0a21c1adad2652380db4ffb450540a4275ccb2
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-07-09 13:49:04 -0700

    research added

--- src/weekThreeHomework/Research.java
+++ src/weekThreeHomework/Research.java
@@ -16,39 +16,49 @@ public class Research {
 		
 		/*
 		 * String: 5 methods
-		 * 1.1) 
-		 * 1.2)
-		 * 1.3)
-		 * 2.1) 
-		 * 2.2)
-		 * 2.3)
-		 * 3.1) 
-		 * 3.2)
-		 * 3.3)
-		 * 4.1) 
-		 * 4.2)
-		 * 4.3)
-		 * 5.1) 
-		 * 5.2)
-		 * 5.3)
+		 * 1.1) public String concat (String str)
+		 * 1.2) This method concatenates the specified string to the end of the string variable
+		 * 1.3) This would be useful for updating String contents, for example: taking user input
+		 * 			for first name and last name to display as one field rather than two.
+		 *  
+		 * 2.1) public String toLowerCase()
+		 * 2.2) This method converts all the characters of a String to lower case
+		 * 2.3) This is useful when analyzing/storing usernames to ensure everything matches
+		 * 
+		 * 3.1) public String toUpperCase
+		 * 3.2) This method converts all the characters in this String to uppercase
+		 * 3.3) This method can be used when you need things to be in uppercase
+		 * 
+		 * 4.1) public String toString()
+		 * 4.2) This returns a string.
+		 * 4.3) When you need to verify that input or data is used as a String
+		 * 
+		 * 5.1) public static String format(String format, Object... args)
+		 * 5.2) This returns a formatted string using the specified format and arguments.
+		 * 5.3) Useful for having data returned or displayed in a specific format, like a telephone number.
 		 * 
 		 * 
 		 * Array: 5 methods
-		 * 1.1) 
-		 * 1.2)
-		 * 1.3)
-		 * 2.1) 
-		 * 2.2)
-		 * 2.3)
-		 * 3.1) 
-		 * 3.2)
-		 * 3.3)
-		 * 4.1) 
-		 * 4.2)
-		 * 4.3)
-		 * 5.1) 
-		 * 5.2)
-		 * 5.3)
+		 * 1.1) public static void sort(int[] a)
+		 * 1.2) This sorts the array into ascending numerical order
+		 * 1.3) Useful when numbers need to be in order
+		 * 
+		 * 2.1) public static boolean equals(int[] a, int[] a2)
+		 * 2.2) This returns true if the two arrays are equal
+		 * 2.3) Equality is often used for logic statements and decision making
+		 * 
+		 * 3.1) public static void fill(int[] a, int value)
+		 * 3.2) This assigns the specified int value to each element of the specified array
+		 * 3.3) This would allow you to dynamically populate your array with specified info
+		 * 
+		 * 4.1) public static String toString(int[] a)
+		 * 4.2) This returns a string representation of your array
+		 * 4.3) This would be valuable when you need to be working with a string
+		 * 
+		 * 5.1) public static int hashCode(int[] a)
+		 * 5.2) Returns a hashcode based on the contents of the array
+		 * 5.3) There are times when you need to identify data and a hashcode is a way of identifying
+		 * 		or indexing the data in the array 
 		 * 
 		 */
 	}
